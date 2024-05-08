452f053240b3917ef43f2b528c80e28501952f15
Yuriy Al. Shirokov <yuriy.shirokov@gmail.com>
2024-05-08 23:28:35 +0300

    A bit more complicated shader

--- resources/test.fs
+++ resources/test.fs
@@ -7,5 +7,7 @@ uniform sampler2D Texture;
 
 void main() {
     gl_FragColor = texture2D(Texture, uv);
-    gl_FragColor.r = gl_FragColor.g * (1.0 - gl_FragColor.b) * 3.0;
+    gl_FragColor.r = gl_FragColor.r + ((gl_FragCoord.x * gl_FragCoord.x)  / 100000.0) - 0.5;
+    gl_FragColor.g = gl_FragColor.g + ((gl_FragCoord.y * gl_FragCoord.y)  / 1000000.0) - 0.5;
+    gl_FragColor.b = 1.0 - (gl_FragColor.b + ((gl_FragCoord.y * gl_FragCoord.y)  / 1000000.0));
 }
