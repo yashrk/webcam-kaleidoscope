1886a38460f6defd159bfaee92be30f9f9ccafbf
Katerina Shananina <serpent.k@gmail.com>
2024-05-08 21:10:33 +0300

    Decoders (#1)
    Decoders added
    
    ---------
    
    Co-authored-by: Katerina N. Shananina <e.shananina@rambler-co.ru>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use ffimage::color::Rgb;
-use ffimage_yuv::yuv::Yuv;
-use ffimage_yuv::yuv422::Yuyv;
 use macroquad::prelude::*;
 use macroquad::texture::Texture2D;
 use v4l::buffer::Type;
@@ -10,27 +7,30 @@ use v4l::video::Capture;
 use v4l::Device;
 use v4l::FourCC;
 
-const WIDTH_U16: u16 = 960;
-const HEIGHT_U16: u16 = 544;
-const WIDTH_U32: u32 = 960;
-const HEIGHT_U32: u32 = 544;
+use webcam::decoder::*;
+
+const WIDTH_U32: u32 = 640;
+const HEIGHT_U32: u32 = 480;
 const BUFFER_COUNT: u32 = 4;
 
 #[macroquad::main("Simple window")]
 async fn main() {
-    let mut dev = Device::new(0).expect("Failed to open device");
+    let dev = Device::new(0).expect("Failed to open device");
     let mut fmt = dev.format().expect("Failed to read format");
-
     fmt.width = WIDTH_U32;
     fmt.height = HEIGHT_U32;
     fmt.fourcc = FourCC::new(b"YVUV");
+    println!("Format to set:\n{}", fmt);
     let fmt = dev.set_format(&fmt).expect("Failed to write format");
-
     println!("Format in use:\n{}", fmt);
-
-    let mut stream = Stream::with_buffers(&mut dev, Type::VideoCapture, BUFFER_COUNT)
+    let decode = if fmt.fourcc == FourCC::new(b"MJPG") {
+        decode_mjpeg
+    } else {
+        decode_yuyv
+    };
+    let mut stream = Stream::with_buffers(&dev, Type::VideoCapture, BUFFER_COUNT)
         .expect("Failed to create buffer stream");
-    let mut image = Image::gen_image_color(WIDTH_U16, HEIGHT_U16, WHITE);
+    let mut image = Image::gen_image_color(fmt.width as u16, fmt.height as u16, WHITE);
     let texture = Texture2D::from_image(&image);
 
     let camera = Camera3D {
@@ -45,36 +45,19 @@ async fn main() {
         // Webcam
         //
         let (buf, _meta) = stream.next().unwrap();
-
         //
         // GUI
         //
+        decode(&mut image, buf);
+        texture.update(&image);
+
+        clear_background(LIGHTGRAY);
+
         set_camera(&camera);
 
-        let image_data = image.get_image_data_mut();
-        for i in 0..(buf.len() / 4) {
-            let b1 = buf[i * 4];
-            let b2 = buf[i * 4 + 1];
-            let b3 = buf[i * 4 + 2];
-            let b4 = buf[i * 4 + 3];
-            let yuyv = Yuyv::<u8>::from([b1, b2, b3, b4]);
-            let yuv = <[Yuv<u8>; 2]>::from(yuyv);
-            let rgb1 = Rgb::<u8>::from(yuv[0]);
-            let r = rgb1[0];
-            let g = rgb1[1];
-            let b = rgb1[2];
-            let alpha = 255;
-            let pixel = vec![r, g, b, alpha];
-            image_data[i * 2] = pixel.try_into().unwrap();
-            let rgb2 = Rgb::<u8>::from(yuv[1]);
-            let r = rgb2[0];
-            let g = rgb2[1];
-            let b = rgb2[2];
-            let alpha = 255;
-            let pixel = vec![r, g, b, alpha];
-            image_data[i * 2 + 1] = pixel.try_into().unwrap();
-        }
-        texture.update(&image);
+        draw_grid(1, 0.1, WHITE, RED);
+        draw_plane(vec3(-8., 0., -8.), vec2(5., 5.), Some(&texture), WHITE);
+
         draw_affine_parallelogram(
             Vec3::ZERO,
             10. * Vec3::X,
@@ -111,12 +94,13 @@ async fn main() {
             WHITE,
         );
         draw_affine_parallelogram(
-            Vec3::ZERO,
+            vec3(5.0, 0., 0.),
             10. * Vec3::NEG_X,
             10. * Vec3::Z,
             Some(&texture),
             WHITE,
         );
+        set_default_camera();
         next_frame().await
     }
 }
