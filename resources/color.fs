5b2ae7fee961e3a0c1c09b495cca59692c7a4a02
Yuriy Al. Shirokov <yuriy.shirokov@gmail.com>
2024-05-09 02:29:29 +0300

    Color saturation shader

---
+++ resources/color.fs
@@ -0,0 +1,13 @@
+#version 100
+precision lowp float;
+
+varying vec2 uv;
+
+uniform sampler2D Texture;
+
+void main() {
+    gl_FragColor = texture2D(Texture, uv);
+    gl_FragColor.r = pow(gl_FragColor.r,3.0);
+    gl_FragColor.g = pow(gl_FragColor.g,3.0);
+    gl_FragColor.b = pow(gl_FragColor.b,3.0);
+}
