c8622deac6d16a96abf4cc96369a310f18037a8f
Yuriy Al. Shirokov <yashrk@users.noreply.github.com>
2024-05-23 00:58:13 +0400

    Error handling improved, new shader (#13)
    Error handling improved, new shader

---
+++ shaders/corners.fs
@@ -0,0 +1,15 @@
+#version 100
+precision mediump float;
+
+varying vec2 uv;
+
+uniform sampler2D Texture;
+
+void main() {
+  vec4 gradientColor;
+  gradientColor.r = uv.x;
+  gradientColor.g = uv.y;
+  gradientColor.b = smoothstep(0.0, 1.0, 1.0 - sqrt(pow(uv.x, 2.0) + pow(uv.y, 2.0)));
+  gradientColor.a = 1.0;
+  gl_FragColor = pow(mix(gradientColor, texture2D(Texture, uv), 0.15), vec4(2.0, 2.0, 2.0, 1.0));
+}
