a10fb3db431bf273bb4a0a778ece9eb69c1f1e0b
Yuriy Shirokov <yshirokov@nvidia.com>
2024-05-24 01:50:28 +0400

    peak() function in rainbow shader improved

--- shaders/rainbow.fs
+++ shaders/rainbow.fs
@@ -6,16 +6,16 @@ varying vec2 uv;
 uniform sampler2D Texture;
 
 float peak(float center, float slope, float height) {
-  return (-pow(uv.x-center, 2.0)+height)*slope;
+  return 1./(pow(slope * uv.x-center, 2.0) + 1.)*height;
 }
 
 void main() {
   vec4 textureColor = texture2D(Texture, uv);
   float meanColor = (textureColor.r + textureColor.g + textureColor.b)/3.0;
   vec4 rainbow;
-  rainbow.r = max(peak(-0.2,0.7,2.0),peak(1.2,1.2,0.3));
-  rainbow.g = peak(0.65,3.2,0.45);
-  rainbow.b = peak(1.1,0.9,1.3);
+  rainbow.r = max(peak(0.,1.5,1.),peak(2.1,2.5,0.5));
+  rainbow.g = peak(1.0,2.5,1.);
+  rainbow.b = peak(1.8,3.,1.3);
   rainbow.a = 1.0;
   gl_FragColor = vec4(meanColor, meanColor, meanColor, 1.0) * rainbow;
 }
