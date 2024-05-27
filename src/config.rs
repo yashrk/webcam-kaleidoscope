d9ef353dc2f4a5d6ccbe31c60dd1d4585da47189
Katerina N. Shananina <e.shananina@rambler-co.ru>
2024-05-27 23:52:52 +0300

    Config

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,14 @@ use serde::Deserialize;
 
 use crate::controls::Keyboard;
 
+#[derive(Deserialize, Clone, Debug, Default)]
+pub struct WebCameraSettings {
+    pub width: u32,
+    pub height: u32
+}
+
 #[derive(Deserialize, Clone, Debug)]
 pub struct Settings {
     pub keyboard: Keyboard,
+    pub webcamera: WebCameraSettings,
 }
